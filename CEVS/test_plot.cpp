947c87b22294f40cb8510d5498af0413695e1e03
--global <--global>
2022-02-03 10:09:00 +0100

    Added logging-solution

---
+++ CEVS/test_plot.cpp
@@ -0,0 +1,16 @@
+#include "matplotlibcpp2.h"
+#include <vector>
+
+using namespace std;
+namespace plt = matplotlibcpp;
+
+int main() {
+    vector<int> x;
+    vector<int> y;
+    for (int i = 0; i < 5; i++) {
+        x.push_back(i);
+        y.push_back(i*i);
+    }
+    plt::plot(x, y);
+    plt::save("./basic.png");
+}
---
+++ CEVS/utility/logging_solution.h
@@ -0,0 +1,12 @@
+#ifndef LOGGING_SOLUTION_H
+#define LOGGING_SOLUTION_H
+
+#include "shallow_solution.h"
+
+using namespace std;
+
+class LoggingSolution : public ShallowSolution {
+
+};
+
+#endif
